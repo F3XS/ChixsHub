dc99d1bc9cfd66a1dd4f7483c0117792c9def194
Chixs <103441440+F3XS@users.noreply.github.com>
2022-06-21 00:36:26 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,17 @@
+if game.PlaceId == 1962086868 then -- // TOWER OF HELL \\ --
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/TowerOfHell')))()
+elseif game.PlaceId == 3582763398 then -- // TOWER OF HELL 2 \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/TowerOfHell')))()
+elseif game.PlaceId == 7167319176 then -- // CHICAGO REMASTERED \\ --
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/ChicagoRemastered')))()
+elseif game.PlaceId == 2768379856 then -- // SCP 3007 \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/3007')))()
+elseif game.PlaceId == 9030362964 then -- // FIELD GOAL SIMULATOR \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/FieldGoalSimulator')))()
+elseif game.PlaceId == 7075737729 then -- // HOOP CENTRAL 6 \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/HoopCentral6')))()
+elseif game.PlaceId == 370731277 then -- // MEEP CITY \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/MeepCity.lua')))()
+else
+    loadstring(game:HttpGet("  https://raw.githubusercontent.com/F3XS/ChixsHub/main/Unsupported"))()
+end
