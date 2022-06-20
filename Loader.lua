51277c9832c57bc536c9a700967931a6942202d8
Chixs <103441440+F3XS@users.noreply.github.com>
2022-06-21 00:59:54 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,7 +8,13 @@ elseif game.PlaceId == 2768379856 then -- // SCP 3007 \\ --
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/3007')))()
 elseif game.PlaceId == 9030362964 then -- // FIELD GOAL SIMULATOR \\ -- 
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/FieldGoalSimulator')))()
-elseif game.PlaceId == 7075737729 then -- // HOOP CENTRAL 6 \\ -- 
+elseif game.PlaceId == 7075737729 then -- // HOOP CENTRAL 6 - MENU \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/HoopCentral6')))()
+elseif game.PlaceId == 7075761003 then -- // HOOP CENTRAL 6 - PARK \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/HoopCentral6')))()
+elseif game.PlaceId == 7230689516 then -- // HOOP CENTRAL 6 - PLAYGROUND \\ -- 
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/HoopCentral6')))()
+elseif game.PlaceId == 7075760303 then -- // HOOP CENTRAL 6 - PRIVATE GYM \\ -- 
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/HoopCentral6')))()
 elseif game.PlaceId == 370731277 then -- // MEEP CITY \\ -- 
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/F3XS/ChixsHub/main/MeepCity.lua')))()
